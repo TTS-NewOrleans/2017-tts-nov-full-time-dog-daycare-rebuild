31f4a12f7ada0c0f1f79d247c8631943eea4dd82
Ryan Hageman <improv42@gmail.com>
2017-11-20 11:11:32 -0600

    Add Breed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+breed_names = ['German Shepherd',
+               'Basset Hound',
+               'Bull Dog',
+               'Dingo',
+               'Catahoula',
+               'Dalmation',
+               'Lab',
+               'Australian Shepherd',
+               'Poodle',
+               'Maltese',
+               'Blue Heeler',
+               'Mutt',
+               'Jack Russel Terrier',
+               'Pit Bull',
+               'Chihuahua',
+               'Golden Retreiver',
+               'Terrier',
+               'Shi-Tzu',
+               'Chow',
+               'Greyhound',
+               'Sled Dog',
+               'Husky',
+               'Yukon Frizz'
+              ]
+
+breed_names.each do |breed_name|
+  Breed.create(name: breed_name)
+end
+
+puts "Your breeds have been seeded with your breed seeds for breeds."
+
+
+
+
